87f6d5252c951a76d9aa4aecb2fe397e60b0d687
igor-pozhidaev-qasunrise <igor@qasunrise.com>
2019-04-13 19:21:19 +0600

    initial NUnit setup
    add NUnit
    add multiple browsers setup
    add google sample test case

---
+++ Emotorwerks.NUnit/Emotorwerks.NUnit/Tests.cs
@@ -0,0 +1,43 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+
+namespace Emotorwerks.NUnit
+{
+    [TestFixture(typeof(ChromeDriver))]
+    [TestFixture(typeof(FirefoxDriver))]
+    [TestFixture(typeof(InternetExplorerDriver))]
+    [Parallelizable]
+
+    public class Tests<TWebDriver> where TWebDriver : IWebDriver, new()
+    {
+        IWebDriver driver;
+        
+        [SetUp]
+        public void Initialize()
+        {
+            this.driver = new TWebDriver();
+        }
+
+        [Test]
+        public void googleTest()
+        {
+            // TODO: Add your test code here
+            driver.Navigate().GoToUrl("https://www.google.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
